5b7f6378283fa9aec1447ff3f1f231ae5bf8c803
Olohireme <oajayi@seamfix.com>
2019-09-26 18:18:33 +0100

    fixed issues with twitter share

--- src/app/view-story/view-story.component.ts
+++ src/app/view-story/view-story.component.ts
@@ -12,6 +12,7 @@ export class ViewStoryComponent implements OnInit {
   content: string;
   story: string[];
   storyTitle: string;
+  url = window.location.href;
 
   constructor(
     private route: ActivatedRoute,
